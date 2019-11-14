83db5905c71c34f0c58053555cde684b21d75457
liuxinhongg <644391351@qq.com>
2019-11-14 17:19:49 +0800

    vuerouter

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    
-    <a href="/">首页</a>
-    <a href="/detail">详情页</a>
+    <!-- 默认的是hash模式 
+      <a href="#/">首页</a>
+    <a href="#/detail">详情页</a> -->
+    <!--history模式
+       <a href="/">首页</a>
+    <a href="/detail">详情页</a> -->
+    <router-link to="/" exact>首页</router-link>
+    <router-link to="/detail" active-class="nav">详情页</router-link>
+    <router-link to="/aa">hello页</router-link>
     <router-view/>
   </div>
 </template>
@@ -27,5 +33,14 @@ export default {
   color: #2c3e50;
   /* margin-top: 60px; */
 }
-
+.router-link-active{
+  background-color: red;
+}
+.nav{
+  background-color: blue;
+}
+.one{
+  background-color: green;
+  color: white;
+}
 </style>
